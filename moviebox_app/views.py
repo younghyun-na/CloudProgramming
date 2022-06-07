3f48eb1b05e3232b24afc51f402a5bf69f802429
younghyun-na <younghyunna@naver.com>
2022-06-07 17:24:52 +0900

    :sparkles: 영화 데이터 가져오기(#2)

---
+++ moviebox_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import requests
+import json
+
+my_id = '11c1b02a6e96fc44d5bc2a59c0dcc332'
+
+def home(request):
+    url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=' + my_id
+    response = requests.get(url)
+    movieData = response.text
+    obj = json.loads(movieData)
+    obj = obj['results']
+    return render(request, 'index.html', {'obj': obj})
+
+
--- moviebox_proj/urls.py
+++ moviebox_proj/urls.py
@@ -1,4 +1,4 @@
-"""moviebox URL Configuration
+"""moviebox_proj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from moviebox_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home")
 ]
