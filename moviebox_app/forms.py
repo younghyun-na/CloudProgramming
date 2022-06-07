b3cbdbfbd117dac8547a8e35d3fb6b44bf5c9b74
younghyun-na <younghyunna@naver.com>
2022-06-08 04:14:05 +0900

    :sparkles: 검색 기능 추가 (#3)

---
+++ moviebox_app/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class SearchForm(forms.Form):
+    search = forms.CharField(label= 'Search Movie', max_length=50)
--- moviebox_app/views.py
+++ moviebox_app/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 import requests
 import json
+from .forms import SearchForm
 
 my_id = '11c1b02a6e96fc44d5bc2a59c0dcc332'
 
 def home(request):
-    url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=' + my_id
-    response = requests.get(url)
-    movieData = response.text
-    obj = json.loads(movieData)
-    obj = obj['results']
-    return render(request, 'index.html', {'obj': obj})
 
+    # 검색 기능 (POST 요청)
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        searchword = request.POST.get('search')
+        if form.is_valid():
+            url = 'https://api.themoviedb.org/3/search/movie?api_key=' + my_id + '&query=' + searchword
+            response = requests.get(url)
+            movieData = response.text
+            movie_list = json.loads(movieData)
+            movie_list = movie_list['results']
+            return render(request, 'search.html', {'movie_list': movie_list})
+
+    # 메인 페이지의 최신 영화 목록 (GET 요청)
+    else:
+        form = SearchForm()
+        url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=' + my_id
+        response = requests.get(url)
+        movieData = response.text
+        movie_list = json.loads(movieData)
+        movie_list = movie_list ['results']
+        context = {'movie_list': movie_list, 'form': form}
+        return render(request, 'index.html', context)
+
+# 영화 정보 상세페이지
 def detail(request, movie_id):
     url = 'https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + my_id
     response = requests.get(url)
