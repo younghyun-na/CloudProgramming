f487bfdcfbffde25bb536aa01b0f2ebdf723ffe4
younghyun-na <younghyunna@naver.com>
2022-06-12 02:04:37 +0900

    :sparkles: 기본 template 추가 (#5)

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,8 @@ def login(request):
 
         user_id = request.POST['user_id']
         user_pw = request.POST['user_pw']
-        user = auth.authenticate(request, user_id = user_id , user_pw=user_pw)
+
+        user = auth.authenticate(request, username = user_id , password=user_pw)
 
         if user is not None:
             auth.login(request, user)
