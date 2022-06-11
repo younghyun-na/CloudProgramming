0e4e745cd7a0151076a3b87deefd13dcbaaf9445
younghyun-na <younghyunna@naver.com>
2022-06-12 03:39:24 +0900

    :sparkles: 메인 화면 추가 (#5)

--- moviebox_app/views.py
+++ moviebox_app/views.py
@@ -36,4 +36,4 @@ def detail(request, movie_id):
     url = 'https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + my_id
     response = requests.get(url)
     movieData = response.text
-    return render(request, 'detail.html', {'movieData': movieData})
+    return render(request, 'detail1.html', {'movieData': movieData})
