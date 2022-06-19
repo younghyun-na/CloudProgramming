289b47f0c098fb4973e8b7c93ff14301aca9e429
younghyun-na <younghyunna@naver.com>
2022-06-19 22:32:24 +0900

    :sparkles: 로그아웃 버튼 추가 (#4)

--- board/forms.py
+++ board/forms.py
@@ -7,3 +7,19 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = '__all__'
         # fields = ('movie_name', 'title', 'content',)
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': "글 제목을 입력해주세요",
+            'rows': 20
+        }
+
+        self.fields['content'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': "글 내용을 입력해주세요",
+            'rows': 20,
+            'cols': 100
+        }
